c7253aea82faed662f07b5ea927a100fdde02596
Stephen Blackstone <sblackstone@gmail.com>
2022-02-11 19:04:04 -0500

    Tests for bishop moves

--- movegenerator/gen_bishop_moves.go
+++ movegenerator/gen_bishop_moves.go
@@ -2,26 +2,61 @@ package movegenerator
 
 import (
 	"github.com/sblackstone/go-chess/boardstate"
-//	"github.com/sblackstone/go-chess/bitopts"
+	"github.com/sblackstone/go-chess/bitopts"
 
 //	"fmt"
 )
 
-func genSingleBishopMoves(b *boardstate.BoardState, rookPos uint8) []*boardstate.BoardState {
+func genSingleBishopMoves(b *boardstate.BoardState, bishopPos uint8) []*boardstate.BoardState {
 	var result []*boardstate.BoardState;
-	/*
 
-	for r := rookPos+8; r < 64; r += 8 {
+	_, file := bitopts.SquareToRankFile(bishopPos)
+
+	for r := bishopPos+9; r < 64 && r % 8 > file; r += 9 {
 			color := b.ColorOfSquare(r)
 			if color == boardstate.EMPTY || color != b.GetTurn() {
-				result = append(result, b.CopyPlayTurn(rookPos, r))
+				result = append(result, b.CopyPlayTurn(bishopPos, r))
 			}
 			if color != boardstate.EMPTY {
 				break;
 			}
-
 	}
 
+	for r := bishopPos+7; r < 64 && r % 8 < file; r += 7 {
+			color := b.ColorOfSquare(r)
+			if color == boardstate.EMPTY || color != b.GetTurn() {
+				result = append(result, b.CopyPlayTurn(bishopPos, r))
+			}
+			if color != boardstate.EMPTY {
+				break;
+			}
+	}
+
+
+	for r := bishopPos-7; r < 64 && r % 8 > file; r -= 7 {
+			color := b.ColorOfSquare(r)
+			if color == boardstate.EMPTY || color != b.GetTurn() {
+				result = append(result, b.CopyPlayTurn(bishopPos, r))
+			}
+			if color != boardstate.EMPTY {
+				break;
+			}
+	}
+
+	for r := bishopPos-9; r < 64 && r % 8 < file; r -= 9 {
+			color := b.ColorOfSquare(r)
+			if color == boardstate.EMPTY || color != b.GetTurn() {
+				result = append(result, b.CopyPlayTurn(bishopPos, r))
+			}
+			if color != boardstate.EMPTY {
+				break;
+			}
+	}
+
+
+
+
+/*
 	for r := rookPos-8; r < 64; r -= 8 {
 		color := b.ColorOfSquare(r)
 		if color == boardstate.EMPTY || color != b.GetTurn() {
--- movegenerator/gen_bishop_moves_test.go
+++ movegenerator/gen_bishop_moves_test.go
@@ -1,1 +1,67 @@
 package movegenerator
+
+import (
+	"testing"
+  "reflect"
+  "github.com/sblackstone/go-chess/boardstate"
+//	"sort"
+
+//  "fmt"
+//	"github.com/sblackstone/go-chess/bitopts"
+
+)
+func TestGenBishopMovesMiddleOfBoard(t *testing.T) {
+  b := boardstate.Blank()
+  b.SetSquare(5, boardstate.WHITE, boardstate.BISHOP)
+  locations := genSortedBoardLocationsBishops(b)
+  expected := []uint8{12, 14, 19, 23, 26, 33, 40}
+  if !reflect.DeepEqual(locations, expected) {
+    t.Errorf("Expected %v to be %v", locations, expected)
+  }
+}
+
+
+
+func TestGenBishopMovesSq42(t *testing.T) {
+  b := boardstate.Blank()
+  b.SetSquare(42, boardstate.WHITE, boardstate.BISHOP)
+  locations := genSortedBoardLocationsBishops(b)
+  expected := []uint8{7, 14, 21, 24, 28, 33, 35, 49, 51, 56, 60}
+  if !reflect.DeepEqual(locations, expected) {
+    t.Errorf("Expected %v to be %v", locations, expected)
+  }
+}
+
+func TestGenBishopMovesSq37(t *testing.T) {
+  b := boardstate.Blank()
+  b.SetSquare(37, boardstate.WHITE, boardstate.BISHOP)
+  locations := genSortedBoardLocationsBishops(b)
+  expected := []uint8{1, 10, 19, 23, 28, 30, 44, 46, 51, 55, 58}
+  if !reflect.DeepEqual(locations, expected) {
+    t.Errorf("Expected %v to be %v", locations, expected)
+  }
+}
+
+func TestGenBishopMovesSqNWCorner(t *testing.T) {
+  b := boardstate.Blank()
+  b.SetSquare(56, boardstate.WHITE, boardstate.BISHOP)
+  locations := genSortedBoardLocationsBishops(b)
+  expected := []uint8{7, 14, 21, 28,35,42,49}
+  if !reflect.DeepEqual(locations, expected) {
+    t.Errorf("Expected %v to be %v", locations, expected)
+  }
+}
+
+func TestGenBishopMovesSqSECorner(t *testing.T) {
+  b := boardstate.Blank()
+  b.SetSquare(7, boardstate.WHITE, boardstate.BISHOP)
+  locations := genSortedBoardLocationsBishops(b)
+	expected := []uint8{14, 21, 28,35,42,49,56}
+  if !reflect.DeepEqual(locations, expected) {
+    t.Errorf("Expected %v to be %v", locations, expected)
+  }
+}
+
+func TestGenBishopMovesKnowsAboutTurns(t *testing.T) {
+	t.Errorf("TODO\n")
+}
