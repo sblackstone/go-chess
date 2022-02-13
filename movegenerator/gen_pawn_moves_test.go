0ccd5fc16126cb936d5331312dbd4cf51d41133f
Stephen Blackstone <sblackstone@gmail.com>
2022-02-13 12:03:03 -0500

    pawn tests

--- movegenerator/gen_pawn_moves_test.go
+++ movegenerator/gen_pawn_moves_test.go
@@ -2,7 +2,7 @@ package movegenerator
 
 import (
 	"testing"
-	"fmt"
+	//"fmt"
   "reflect"
   "github.com/sblackstone/go-chess/boardstate"
 )
@@ -18,13 +18,45 @@ func TestGenPawnMovesUnderstandsTurn(t *testing.T) {
 
 func TestPushPawnWhite(t *testing.T) {
   b := boardstate.Blank()
-  b.SetSquare(27, boardstate.WHITE, boardstate.PAWN)
-  locations := genSortedBoardLocationsPawns(b)
+	b.SetSquare(27, boardstate.WHITE, boardstate.PAWN)
+	locations := genSortedBoardLocationsPawns(b)
   expected := []uint8{35}
   if !reflect.DeepEqual(locations, expected) {
     t.Errorf("Expected %v to be %v", locations, expected)
   }
+
+	// Obstructed by SELF
+	b.SetSquare(35, boardstate.WHITE, boardstate.QUEEN)
+	var expected2 []uint8
+	locations2 := genSortedBoardLocationsPawns(b)
+
+  if !reflect.DeepEqual(locations2, expected2) {
+    t.Errorf("Expected %v to be %v", locations2, expected2)
+  }
+
+	/// Obstructed by ENEMY
+	b.SetSquare(35, boardstate.BLACK, boardstate.QUEEN)
+	var expected3 []uint8
+	locations3 := genSortedBoardLocationsPawns(b)
+
+  if !reflect.DeepEqual(locations3, expected3) {
+    t.Errorf("Expected %v to be %v", locations3, expected3)
+  }
+
+
+
 }
+
+func TestPushPawnTwoWhite(t *testing.T) {
+  b := boardstate.Blank()
+  b.SetSquare(8, boardstate.WHITE, boardstate.PAWN)
+  locations := genSortedBoardLocationsPawns(b)
+  expected := []uint8{16,24}
+  if !reflect.DeepEqual(locations, expected) {
+    t.Errorf("Expected %v to be %v", locations, expected)
+  }
+}
+
 
 func TestPushPawnPromoteWhite(t *testing.T) {
   b := boardstate.Blank()
