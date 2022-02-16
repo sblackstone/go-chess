9b23a1fa0676465b1b9a3d433c974e554422f4ae
Stephen Blackstone <sblackstone@gmail.com>
2022-02-16 15:45:01 -0500

    incomplete fenparser

---
+++ boardstate/fenparser_test.go
@@ -0,0 +1,16 @@
+package boardstate
+
+import (
+  "testing"
+  "fmt"
+)
+
+func TestFENParserDefaultBoard(t *testing.T) {
+  testStr := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  b, err := FromFEN(testStr)
+  if err != nil {
+    fmt.Printf("%v\n", err)
+  } else {
+    b.Print(125)
+  }
+}
