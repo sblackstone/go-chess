3047d05c8e75e44cc356f44f33e5fbda29fab9d7
Stephen Blackstone <sblackstone@gmail.com>
2022-06-28 18:12:29 -0400

    latest

--- movegenerator/perft_test.go
+++ movegenerator/perft_test.go
@@ -4,10 +4,7 @@ import (
   "testing"
 	"github.com/sblackstone/go-chess/boardstate"
   "github.com/sblackstone/go-chess/fen"
-  //"reflect"
 )
-
-
 
 func genPerf(state *boardstate.BoardState, depth int) int {
   successors := GenLegalSuccessors(state)
@@ -23,82 +20,32 @@ func genPerf(state *boardstate.BoardState, depth int) int {
 }
 
 
-func TestPerft(t *testing.T) {
-  b := boardstate.Initial()
-  result := genPerf(b, 3)
-  t.Errorf("%v\n", result)
-
-}
-
-
-// https://www.chessprogramming.org/Perft_Results
-
-func TestPerftPosition2(t *testing.T) {
-  b, err := fen.FromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0")
-
-  if err != nil {
-    t.Errorf("%v", err)
-    return
+func TestPerftPositions(t *testing.T) {
+  var perfTests = []struct {
+    name string
+    fen string
+    depth int
+    expected int
+  }{
+    { "Initial Position", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4, 197281},
+    { "Position 2", "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 0", 4, 4085603},
+    { "Position 3", "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0", 5, 674624},
+    { "Position 4A", "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 4, 422333},
+    { "Position 4B", "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1", 4, 422333},
+    { "Position 5", "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", 3, 62379},
+    { "Position 6", "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", 4, 3894594},
   }
-  b.Print(127)
-  result := genPerf(b, 3)
-  t.Errorf("%v\n", result)
 
-
-}
-
-func TestPosition3(t *testing.T) {
-  b, err := fen.FromFEN("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0")
-
-  if err != nil {
-    t.Errorf("%v", err)
-    return
+  for _, pt := range perfTests {
+    b, err := fen.FromFEN(pt.fen)
+    if err != nil {
+      t.Errorf("%v\n", err)
+    } else {
+      result := genPerf(b, pt.depth)
+      if (result != pt.expected) {
+        t.Errorf("%v: Expected %v, got %v", pt.fen, pt.expected, result)
+      }
+    }
   }
-  b.Print(127)
-  result := genPerf(b, 3)
-  t.Errorf("%v\n", result)
-
-
-}
-
-
-func TestPosition4(t *testing.T) {
-  b, err := fen.FromFEN("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")
-
-  if err != nil {
-    t.Errorf("%v", err)
-    return
-  }
-  b.Print(127)
-  result := genPerf(b, 3)
-  t.Errorf("%v\n", result)
-
-
-}
-
-func TestPosition5(t *testing.T) {
-  b, err := fen.FromFEN("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")
-
-  if err != nil {
-    t.Errorf("%v", err)
-    return
-  }
-  b.Print(127)
-  result := genPerf(b, 3)
-  t.Errorf("%v\n", result)
-
-}
-
-
-func TestPosition6(t *testing.T) {
-  b, err := fen.FromFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10")
-
-  if err != nil {
-    t.Errorf("%v", err)
-    return
-  }
-  b.Print(127)
-  result := genPerf(b, 3)
-  t.Errorf("%v\n", result)
 
 }
