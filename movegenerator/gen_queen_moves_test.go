703baf1d622dd63a3f9b89378ea4c6a25ad296e6
Stephen Blackstone <sblackstone@gmail.com>
2022-06-28 09:50:39 -0400

    latest

--- movegenerator/gen_queen_moves_test.go
+++ movegenerator/gen_queen_moves_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
   "reflect"
   "github.com/sblackstone/go-chess/boardstate"
-	"github.com/sblackstone/go-chess/fen"
-	"fmt"
+//	"github.com/sblackstone/go-chess/fen"
+//	"fmt"
 )
 
 
@@ -53,36 +53,36 @@ func TestGenQueenMovesMiddleOfBoard(t *testing.T) {
 //R1Q4R/3Q4/1Q4Q1/4Q3/7Q/Q4Q2/pp1Q4/kBNN1KB1 b - - 1 1
 
 
-// Used to test bug...
-func TestFromFamous(t *testing.T) {
-
-	b, _ := fen.FromFEN("R1Q4R/3Q4/1Q4Q1/4Q3/7Q/Q4Q2/pp1Q4/kBNN1KB1 b - - 1 1")
-
-	qms := genAllQueenMoves(b, boardstate.WHITE)
-
-	for q := range(qms) {
-		move := qms[q]
-		if move.Dst() == 18 {
-			fmt.Printf("%v\n", move)
-			// newBoard := b.CopyPlayTurnFromMove(move)
-			// newBoard.Print(127)
-		}
-	}
-
-	// locations := genSortedBoardLocationsQueens(b)
-	//
-	// expected := []int8{2, 9, 17, 18, 24, 25, 32, 40, 48 }
-  // if !reflect.DeepEqual(locations, expected) {
-  //   t.Errorf("Expected %v to be %v", locations, expected)
-  // }
-	//
-	// positions := genQueenSuccessors(b)
-	// for p := range(positions) {
-	// 	positions[p].Print(127)
-	// }
-	//
-	//
-	//
-	// b.Print(127)
-	t.Errorf("a")
-}
+// // Used to test bug...
+// func TestFromFamous(t *testing.T) {
+//
+// 	b, _ := fen.FromFEN("R1Q4R/3Q4/1Q4Q1/4Q3/7Q/Q4Q2/pp1Q4/kBNN1KB1 b - - 1 1")
+//
+// 	qms := genAllQueenMoves(b, boardstate.WHITE)
+//
+// 	for q := range(qms) {
+// 		move := qms[q]
+// 		if move.Dst() == 18 {
+// 			fmt.Printf("%v\n", move)
+// 			// newBoard := b.CopyPlayTurnFromMove(move)
+// 			// newBoard.Print(127)
+// 		}
+// 	}
+//
+// 	// locations := genSortedBoardLocationsQueens(b)
+// 	//
+// 	// expected := []int8{2, 9, 17, 18, 24, 25, 32, 40, 48 }
+//   // if !reflect.DeepEqual(locations, expected) {
+//   //   t.Errorf("Expected %v to be %v", locations, expected)
+//   // }
+// 	//
+// 	// positions := genQueenSuccessors(b)
+// 	// for p := range(positions) {
+// 	// 	positions[p].Print(127)
+// 	// }
+// 	//
+// 	//
+// 	//
+// 	// b.Print(127)
+// 	t.Errorf("a")
+// }
