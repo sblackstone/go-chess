648d4cac0e5848291df58b1cd2800417a5d16d36
Stephen Blackstone <sblackstone@gmail.com>
2022-06-29 22:54:12 -0400

    latest perft test

--- movegenerator/perft_test.go
+++ movegenerator/perft_test.go
@@ -23,13 +23,13 @@ func genPerf(state *boardstate.BoardState, depth int) int {
 func BenchmarkPerf(b *testing.B) {
   board, _ := fen.FromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
   for n := 0; n < b.N; n++ {
-    genPerf(board, 4)
+    genPerf(board, 6)
   }
 }
 
 func TestSpeed(t *testing.T) {
   board, _ := fen.FromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-  result := genPerf(board, 4)
+  result := genPerf(board, 5)
   t.Errorf("%v", result)
 }
 
