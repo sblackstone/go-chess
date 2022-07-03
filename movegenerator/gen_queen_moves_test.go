7ab09ede895437953da4009126d454b5b2c7115d
Stephen Blackstone <sblackstone@gmail.com>
2022-07-03 09:34:17 -0400

    Upgrade queen moves

--- movegenerator/gen_queen_moves_test.go
+++ movegenerator/gen_queen_moves_test.go
@@ -2,10 +2,7 @@ package movegenerator
 
 import (
 	"testing"
-  "reflect"
   "github.com/sblackstone/go-chess/boardstate"
-//	"github.com/sblackstone/go-chess/fen"
-//	"fmt"
 )
 
 
@@ -17,19 +14,16 @@ func TestGenQueenMovesUnderstandsTurn(t *testing.T) {
 	b := boardstate.Blank()
 	b.SetSquare(56,  boardstate.WHITE, boardstate.QUEEN)
 	b.SetSquare(7,   boardstate.BLACK, boardstate.QUEEN)
-	moves := genSortedBoardLocationsQueens(b)
 
 	expected := []int8{0, 7, 8, 14, 16, 21, 24, 28, 32, 35, 40, 42, 48, 49, 57, 58, 59, 60, 61, 62, 63}
-	if (!reflect.DeepEqual(moves, expected)) {
-		t.Errorf("Expected %v to be %v", moves, expected)
-	}
+	testSuccessorsHelper(t, b, boardstate.QUEEN, expected)
+	testAttacksHelper(t, b, boardstate.QUEEN, expected)
+
 
 	b.ToggleTurn()
-	movesBlack := genSortedBoardLocationsQueens(b)
 	expectedBlack := []int8{0, 1, 2, 3, 4, 5, 6, 14, 15, 21, 23, 28, 31, 35, 39, 42, 47, 49, 55, 56, 63}
-	if (!reflect.DeepEqual(movesBlack, expectedBlack)) {
-		t.Errorf("Expected %v to be %v", movesBlack, expected)
-	}
+	testSuccessorsHelper(t, b, boardstate.QUEEN, expectedBlack)
+	testAttacksHelper(t, b, boardstate.QUEEN, expectedBlack)
 
 }
 
@@ -37,13 +31,9 @@ func TestGenQueenMovesUnderstandsTurn(t *testing.T) {
 func TestGenQueenMovesMiddleOfBoard(t *testing.T) {
   b := boardstate.Blank()
   b.SetSquare(27, boardstate.WHITE, boardstate.QUEEN)
-  locations := genSortedBoardLocationsQueens(b)
   expected := []int8{0, 3, 6, 9, 11, 13, 18, 19, 20, 24, 25, 26, 28, 29, 30, 31, 34, 35, 36, 41, 43, 45, 48, 51, 54, 59, 63}
-  if !reflect.DeepEqual(locations, expected) {
-    t.Errorf("Expected %v to be %v", locations, expected)
-  }
 
-	for i := range(expected) {
-		b.SetSquare(expected[i], boardstate.WHITE, boardstate.QUEEN)
-	}
+	testSuccessorsHelper(t, b, boardstate.QUEEN, expected)
+	testAttacksHelper(t, b, boardstate.QUEEN, expected)
+
 }
