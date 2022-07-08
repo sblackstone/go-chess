9afd8268e12676ba58261e98f3210965638a32e2
Stephen Blackstone <sblackstone@gmail.com>
2022-07-08 16:18:53 -0400

    main.go - some work towards being able to play via UCI

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/sblackstone/go-chess/bitopts"
+	"github.com/sblackstone/go-chess/boardstate"
+	"github.com/sblackstone/go-chess/fen"
+	"github.com/sblackstone/go-chess/treesearch"
 )
 
 func main() {
-	logFile, _ := os.Create("/tmp/go-chess-log")
+	logFile, _ := os.OpenFile("/tmp/go-chess-log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	buf := bufio.NewScanner(os.Stdin)
+
+	var board *boardstate.BoardState
+	var err error
 
 	sendReply := func(line string) {
 		logFile.WriteString(line + "\n")
@@ -32,6 +41,35 @@ func main() {
 
 		if command == "isready" {
 			sendReply("readyok")
+		}
+
+		if strings.HasPrefix(command, "go ") {
+			move := treesearch.BestSuccessor(board, 3)
+			logFile.WriteString(fmt.Sprintf("Best move: %v\n", move))
+		}
+
+		if strings.HasPrefix(command, "position") {
+			command = command[9:]
+			fenStr, moves, hasMoves := strings.Cut(command, "moves")
+			fenStr = strings.Trim(fenStr, " ")
+			moves = strings.Trim(moves, " ")
+			if fenStr == "startpos" {
+				board = boardstate.Initial()
+			} else {
+				board, err = fen.FromFEN(fenStr)
+				if err != nil {
+					logFile.WriteString(fmt.Sprintf("%v\n", err))
+				}
+			}
+			if hasMoves {
+				moveList := strings.Split(strings.ToLower(moves), " ")
+				for _, moveStr := range moveList {
+					src, _ := bitopts.AlgebraicToSquare(moveStr[0:2])
+					dst, _ := bitopts.AlgebraicToSquare(moveStr[2:4])
+					logFile.WriteString(fmt.Sprintf("moveStr: %v src=%v dst=%v\n", moveStr, src, dst))
+					board.PlayTurn(src, dst, boardstate.EMPTY) // TODO: Promotion
+				}
+			}
 		}
 	}
 }
