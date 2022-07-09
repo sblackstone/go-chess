27f37ac109696e81cea5f6d46f9d869df5525ff8
Stephen Blackstone <sblackstone@gmail.com>
2022-07-08 23:54:09 -0400

    BoardFromUCIPosition

--- uci/board.go
+++ uci/board.go
@@ -1,20 +1,42 @@
 package uci
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/sblackstone/go-chess/boardstate"
+	"github.com/sblackstone/go-chess/fen"
 )
 
 func RemovePositionPrefix(positionStr string) string {
-	if strings.HasPrefix(positionStr, "position") {
-		positionStr = positionStr[8:]
-	}
-	return strings.Trim(positionStr, " ")
-
+	return strings.Trim(strings.TrimPrefix(positionStr, "position"), " ")
 }
 
 func BoardFromUCIPosition(positionStr string) *boardstate.BoardState {
 	positionStr = RemovePositionPrefix(positionStr)
-	return boardstate.Initial()
+	fenStr, moves, hasMoves := strings.Cut(positionStr, "moves")
+	var board *boardstate.BoardState
+	var err error
+	fenStr = strings.Trim(fenStr, " ")
+	moves = strings.Trim(moves, " ")
+	if fenStr == "startpos" {
+		board = boardstate.Initial()
+	} else {
+		board, err = fen.FromFEN(fenStr)
+		if err != nil {
+			panic(fmt.Sprintf("%v\n", err))
+		}
+	}
+	if hasMoves {
+		moveList := strings.Split(strings.ToLower(moves), " ")
+		for _, moveStr := range moveList {
+			move, err := MoveFromUCI(moveStr)
+			if err != nil {
+				panic(fmt.Sprintf("Unknown moveStr: %s", moveStr))
+			}
+			board.PlayTurnFromMove(move)
+		}
+	}
+
+	return board
 }
--- uci/board_test.go
+++ uci/board_test.go
@@ -1,6 +1,12 @@
 package uci
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sblackstone/go-chess/boardstate"
+	"github.com/sblackstone/go-chess/fen"
+)
 
 func TestRemovePositionPrefix(t *testing.T) {
 	if RemovePositionPrefix("position blarg") != "blarg" {
@@ -13,6 +19,59 @@ func TestRemovePositionPrefix(t *testing.T) {
 
 	if RemovePositionPrefix("  blarg  ") != "blarg" {
 		t.Errorf("expected %v to be %v", RemovePositionPrefix("position blarg"), "blarg")
+	}
+
+}
+
+func TestBoardFromUCIPosition(t *testing.T) {
+	b1 := boardstate.Initial()
+	b1.PlayTurn(8, 24, boardstate.EMPTY)
+	b1.PlayTurn(55, 39, boardstate.EMPTY)
+
+	b2 := BoardFromUCIPosition("position startpos moves a2a4 h7h5")
+
+	f1, err := fen.ToFEN(b1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	f2, err := fen.ToFEN(b2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if f1 != f2 {
+		t.Errorf("expected %v to be %v", f2, f1)
+	}
+
+}
+
+func TestBoardFromUCIPositionNonStart(t *testing.T) {
+	b1 := boardstate.Initial()
+	b1.PlayTurn(8, 24, boardstate.EMPTY)
+	b1.PlayTurn(55, 39, boardstate.EMPTY)
+
+	f0, err := fen.ToFEN(b1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	posStr := fmt.Sprintf("position %s moves c2c4", f0)
+	b1.PlayTurn(10, 26, boardstate.EMPTY)
+	b2 := BoardFromUCIPosition(posStr)
+
+	f1, err := fen.ToFEN(b1)
+
+	if err != nil {
+		t.Error(err)
+	}
+	f2, err := fen.ToFEN(b2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if f1 != f2 {
+		t.Errorf("Expected %v to be %v", f2, f1)
 	}
 
 }
