c5462ef40b655a5da61b8ed9d2ad94e59de1414e
Stephen Blackstone <sblackstone@gmail.com>
2022-07-10 08:08:05 -0400

    Add more failing tests for unplay turn

--- boardstate/playturn_test.go
+++ boardstate/playturn_test.go
@@ -2,6 +2,22 @@ package boardstate
 
 import "testing"
 
-func TestUnplayTurn(t *testing.T) {
+func TestUnplayTurnPieceMove(t *testing.T) {
 	t.Errorf("Unimplmemented")
+}
+
+func TestUnplayTurnCaptureMove(t *testing.T) {
+	t.Errorf("Unimplmemented")
+}
+
+func TestUnplayTurnEnpassantCapture(t *testing.T) {
+	t.Errorf("Unimplmemented")
+}
+
+func TestUnplayTurnCastling(t *testing.T) {
+	t.Errorf("Unimplmemented")
+}
+
+func TestUnplayTurnPromotion(t *testing.T) {
+	t.Errorf("unimplmeneted")
 }
