5cc2c76652f02f69ee2667f751e9b87457e1ba70
Stephen Blackstone <sblackstone@gmail.com>
2022-07-12 06:40:47 -0400

    save test code, need refactor first

--- boardstate/playturn_test.go
+++ boardstate/playturn_test.go
@@ -2,9 +2,22 @@ package boardstate
 
 import "testing"
 
-func TestUnplayTurnPieceMove(t *testing.T) {
-	t.Errorf("Unimplmemented")
-}
+// import (
+// 	"testing"
+
+// 	"github.com/sblackstone/go-chess/fen"
+// )
+
+// func TestUnplayTurnPieceMove(t *testing.T) {
+// 	b := Initial()
+// 	before := fen.ToFEN(b)
+// 	b.PlayTurn(8, 24)
+// 	b.UnplayTurn()
+// 	after := fen.ToFEN(b)
+// 	if before != after {
+// 		t.Errorf("Expected %v to be %v", after, before)
+// 	}
+// }
 
 func TestUnplayTurnCaptureMove(t *testing.T) {
 	t.Errorf("Unimplmemented")
