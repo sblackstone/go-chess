d5994dd092fe77c9f1e726bd4d11354062a60a1c
Stephen Blackstone <sblackstone@gmail.com>
2022-07-14 10:32:03 -0400

    subset bit twiddling goes in its own file

--- bitops/bitops.go
+++ bitops/bitops.go
@@ -19,28 +19,6 @@ func init() {
 	}
 }
 
-// https://www.chessprogramming.org/Traversing_Subsets_of_a_Set
-func SNOOB(x uint64) uint64 {
-	smallest := x & -x
-	ripple := x + smallest
-	ones := x ^ ripple
-	ones = (ones >> 2) / smallest
-	return ripple | ones
-}
-
-// Enumerates all sets with the same bits as d
-// will not include zero.
-func Subsets(d uint64, callback func(uint64)) {
-	var n uint64 = 0
-	for {
-		n = (n - d) & d
-		if n == 0 {
-			break
-		}
-		callback(n)
-	}
-}
-
 func Mask(pos int8) uint64 {
 	return setMasks[pos]
 }
