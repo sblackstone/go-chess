d89bb05108838e96bbdb86ae6dcbce9b5a60d734
Stephen Blackstone <sblackstone@gmail.com>
2022-07-15 10:22:50 -0400

    magic bitboards

--- movegenerator/magic_bitboards.go
+++ movegenerator/magic_bitboards.go
@@ -1,11 +1,15 @@
 package movegenerator
 
 import (
+	"fmt"
+	"math/bits"
+	"math/rand"
+
 	"github.com/sblackstone/go-chess/bitops"
 	"github.com/sblackstone/go-chess/boardstate"
 )
 
-func RookOccuppancyMasksForSquare(n int8) []uint64 {
+func RookBlockerMasksForSquare(n int8) []uint64 {
 	var result []uint64
 	rank, file := bitops.SquareToRankFile(n)
 	baseMask := (bitops.FileMask(file) | bitops.RankMask(rank))
@@ -37,4 +41,64 @@ func RookAttackSetForOccupancy(n int8, occupancy uint64) uint64 {
 	genSingleRookMovesGeneric(b, n, f)
 	return result
 
+}
+
+type MagicDefinition struct {
+	square     int8
+	mapping    [32768]uint64
+	magicValue uint64
+	rotate     int8
+	preMask    uint64
+}
+
+func RookFindMagic(n int8) *MagicDefinition {
+	blockers := RookBlockerMasksForSquare(n)
+	file, rank := bitops.SquareToRankFile(n)
+	preMask := (bitops.FileMask(file) | bitops.RankMask(rank))
+	blockerBits := bits.OnesCount64(preMask) - 1
+	fmt.Printf("Blocker Bits = %v\n", blockerBits)
+	attackSets := make([]uint64, len(blockers))
+	totalCount := len(blockers)
+	best := 0
+	var magicValue uint64
+	for i, blocker := range blockers {
+		attackSets[i] = RookAttackSetForOccupancy(n, blocker)
+	}
+
+	for {
+		var mapping [32768]uint64
+		magicValue = rand.Uint64() & rand.Uint64() & rand.Uint64()
+		for i, blocker := range blockers {
+			cacheKey := (blocker * magicValue) >> (64 - blockerBits - 1)
+			attackSet := attackSets[i]
+			if mapping[cacheKey] == 0 {
+				mapping[cacheKey] = attackSet
+			}
+			if mapping[cacheKey] != attackSet {
+				if i > best {
+					best = i
+					fmt.Printf("Collision detected at %v of %v with magic %v at cache key %v\n", i, totalCount, magicValue, cacheKey)
+				}
+				break
+			}
+			if i == totalCount-1 {
+				return &MagicDefinition{
+					square:     n,
+					mapping:    mapping,
+					magicValue: magicValue,
+					rotate:     int8(64 - blockerBits - 1),
+					preMask:    preMask,
+				}
+			}
+		}
+	}
+}
+
+func GenerateRookMagicBitboards() [64]*MagicDefinition {
+	var result [64]*MagicDefinition
+	var i int8
+	for i = 0; i < 64; i++ {
+		result[i] = RookFindMagic(i)
+	}
+	return result
 }
--- movegenerator/magic_bitboards_test.go
+++ movegenerator/magic_bitboards_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/sblackstone/go-chess/bitops"
 )
 
-func TestRookOccuppancyMasksForSquare(t *testing.T) {
-	bbs := RookOccuppancyMasksForSquare(17)
+func TestRookBlockerMasksForSquare(t *testing.T) {
+	bbs := RookBlockerMasksForSquare(17)
 	for _, bb := range bbs {
 		bitops.Print(bb, 17)
 		fmt.Printf("\n\n")
@@ -17,7 +17,7 @@ func TestRookOccuppancyMasksForSquare(t *testing.T) {
 }
 
 func TestRookAttackSetForOccupancy(t *testing.T) {
-	// bbs := RookOccuppancyMasksForSquare(0)
+	// bbs := RookBlockerMasksForSquare(0)
 	// fmt.Printf("len = %v\n", len(bbs))
 	// for _, bb := range bbs {
 	// 	attackSet := RookAttackSetForOccupancy(0, bb)
@@ -29,4 +29,18 @@ func TestRookAttackSetForOccupancy(t *testing.T) {
 	// }
 	t.Errorf("not implemented")
 
+}
+
+func TestRookFindMagic(t *testing.T) {
+	var i int8
+	var result [64]*MagicDefinition
+	for i = 0; i < 64; i++ {
+		result[i] = RookFindMagic(i)
+	}
+	t.Errorf("Done")
+}
+
+func TestGenerateRookMagicBitboards(t *testing.T) {
+	result := GenerateRookMagicBitboards()
+	fmt.Printf("%v", result)
 }
