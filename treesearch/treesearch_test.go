8e83b2ae3a6fb77f7a5b7912b500cf47150b47f7
Stephen Blackstone <sblackstone@gmail.com>
2022-08-23 08:45:12 -0400

    Update test treesearch that was failing

--- treesearch/treesearch_test.go
+++ treesearch/treesearch_test.go
@@ -1,11 +1,9 @@
 package treesearch
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/sblackstone/go-chess/boardstate"
-	"github.com/sblackstone/go-chess/uci"
 )
 
 func TestBestSuccessor1(t *testing.T) {
@@ -17,17 +15,18 @@ func TestBestSuccessor1(t *testing.T) {
 	bs.Print(127)
 }
 
-func TestCrash(t *testing.T) {
+// Old test for when no king on board caused a crash.
+// func TestCrash(t *testing.T) {
 
-	b := uci.BoardFromUCIPosition("position fen r5rk/5p1p/5R2/4B3/8/8/7P/7K w - - 0 1 moves f6f7 g8g7 f7g7 a8a5 g7g5 a5e5 g5e5 h7h6 h1g1 h8g7 e5a5 g7h7 a5b5 h7g6 g1f2")
-	fen, _ := b.ToFEN()
-	fmt.Printf("%s\n", fen)
-	bm := BestMove(b, 2)
-	bs := BestSuccessor(b, 2)
-	bs.Print(127)
-	t.Errorf("%v\n", uci.MoveToUCI(bm))
+// 	b := uci.BoardFromUCIPosition("position fen r5rk/5p1p/5R2/4B3/8/8/7P/7K w - - 0 1 moves f6f7 g8g7 f7g7 a8a5 g7g5 a5e5 g5e5 h7h6 h1g1 h8g7 e5a5 g7h7 a5b5 h7g6 g1f2")
+// 	fen, _ := b.ToFEN()
+// 	fmt.Printf("%s\n", fen)
+// 	bm := BestMove(b, 2)
+// 	bs := BestSuccessor(b, 2)
+// 	bs.Print(127)
+// 	t.Errorf("%v\n", uci.MoveToUCI(bm))
 
-}
+// }
 
 // func TestPlayItself(t *testing.T) {
 // 	b := boardstate.Initial()
