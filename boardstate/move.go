dd25b6b14c6cc2f8ec1f97090623e15764639e5b
Stephen Blackstone <sblackstone@gmail.com>
2023-01-09 10:04:01 -0500

    Add another test positon for black to treesearch test

--- boardstate/move.go
+++ boardstate/move.go
@@ -1,7 +1,13 @@
 package boardstate
 
+import "fmt"
+
 type Move struct {
 	Src          int8
 	Dst          int8
 	PromotePiece int8
+}
+
+func (m *Move) Print() {
+	fmt.Printf("%s %s %d", SquareToAlgebraic(m.Src), SquareToAlgebraic(m.Dst), m.PromotePiece)
 }
--- treesearch/treesearch_test.go
+++ treesearch/treesearch_test.go
@@ -15,3 +15,11 @@ func TestCanFindMateInOneForWhite(t *testing.T) {
 	assert.Equal(t, bm.Src, int8(21))
 	assert.Equal(t, bm.Dst, int8(53))
 }
+
+func TestCanFindMateInThreeForBlack(t *testing.T) {
+	b, err := boardstate.FromFEN("2r3k1/p4p2/3Rp2p/1p2P1pK/8/1P4P1/P3Q2P/1q6 b - - 0 1")
+	require.Nil(t, err)
+	bm := BestMove(b, 4)
+	assert.Equal(t, bm.Src, int8(1))
+	assert.Equal(t, bm.Dst, int8(46))
+}
